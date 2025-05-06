bdae9a44f6653b1f5d7039994d47cc8093345c0e
Ruan Necker <86020838+necker1400@users.noreply.github.com>
2025-05-06 09:52:50 +0000

    initial commit

---
+++ acmerent-api/src/test/java/com/acmerent/acmerent_api/AcmerentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acmerent.acmerent_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmerentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
